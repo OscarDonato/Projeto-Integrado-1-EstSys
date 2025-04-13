22290933e9941a43a050275615689089a6b377fc
oscar donato <donato.oscar13@gmail.com>
2025-04-13 20:17:45 -0300

    subindo o que foi feito ate o momento da integração

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from flask import Flask, request, redirect, url_for, jsonify, render_template
 from flask_cors import CORS
+import psycopg2
+from psycopg2 import sql
 
-produtos = []
-services = []
+
+#produtos = []
+#services = []
 
 app = Flask(__name__)
 CORS(app)  # permite que o frontend chame o backend, especialmente útil localmente
@@ -14,6 +17,292 @@ def index():
 @app.route('/cadastros')
 def mensagem():
     return render_template('cadastros.html')
+
+
+##################################################################################################################################################
+##################################################################################################################################################
+##################################################################################################################################################
+##################################################################################################################################################
+# Configurações do banco de dados
+DB_HOST = "localhost"
+DB_NAME = "estetsys"
+DB_USER = "estetsys"
+DB_PASS = "estetsys"
+
+# Função para conectar ao banco de dados do Estetsys como 
+def get_db_connection():
+    conn = psycopg2.connect(host=DB_HOST, database=DB_NAME, user=DB_USER, password=DB_PASS)
+    return conn
+
+########################    Seção de Cliente   ########################
+
+# Rota para adicionar um produto ao cadastro de produtos
+@app.route('/add_cliente', methods=['POST'])
+def add_cliente():
+    data = request.form
+    ad_CLI_CODIGO = data.get('xxxxx', '').strip()
+	ad_CLI_NOME = data.get('xxxxx', '').strip()
+	ad_CLI_ENDERECO	= data.get('xxxxx', '').strip()
+	ad_CLI_TEL = data.get('xxxxx', '').strip()
+	ad_CLI_DOC = data.get('xxxxx', '').strip()
+	ad_CLI_OBSERVA = data.get('xxxxx', '').strip()
+
+    if not ad_CLI_CODIGO or not ad_CLI_NOME or not ad_CLI_ENDERECO or not ad_CLI_TEL or not ad_CLI_DOC:
+        return "Dados incompletos", 400
+    
+    conn = get_db_connection()
+    cur = conn.cursor()
+    
+    try:
+        # Chamando a procedure de inclusão de dados na tabela de cadastro de produtos
+        cur.callproc( 'add_cliente', (ad_CLI_CODIGO, ad_CLI_NOME, ad_CLI_ENDERECO, ad_CLI_TEL, ad_CLI_DOC, ad_CLI_OBSERVA ))
+        conn.commit()
+        response = {"message": "Produto adicionado com sucesso!"}
+    except Exception as e:
+        response = {"error": str(e)}
+        conn.rollback()
+    finally:
+        cur.close()
+        conn.close()
+    
+    return jsonify(response)
+
+
+# Rota para alterar um cadastro de um produto
+@app.route('/alte_produto', methods=['POST'])
+def alte_produto():
+    data = request.form
+    alt_PRD_CODIGO  = data.get('productName', '').strip()
+    alt_PRD_PRECO   = float( data.get('productPrice', '').strip() )
+    alt_PRD_OBSERVA = data.get('productName', '').strip()
+
+    if not alt_PRD_CODIGO or ( not alt_PRD_PRECO and not alt_PRD_OBSERVA ):
+        return "Dados incompletos", 400
+    
+    conn = get_db_connection()
+    cur = conn.cursor()
+    
+    try:
+        # Chamando a procedure de alteração de dados na tabela de cadastro de produtos
+        cur.callproc( 'alte_produto', (alt_PRD_CODIGO, alt_PRD_PRECO, alt_PRD_OBSERVA ) )
+        conn.commit()
+        response = {"message": "Produto alterados com sucesso!"}
+    except Exception as e:
+        response = {"error": str(e)}
+        conn.rollback()
+    finally:
+        cur.close()
+        conn.close()
+    
+    return jsonify(response)
+
+
+# Rota para deletar um cadastro de um produto
+@app.route('/dlt_produto', methods=['POST'])
+def dlt_produto():
+    data = request.form
+    dlt_PRD_CODIGO  = data.get('productName', '').strip()
+
+    if not dlt_PRD_CODIGO
+        return "Dados incompletos", 400
+    
+    conn = get_db_connection()
+    cur = conn.cursor()
+    
+    try:
+        # Chamando a procedure de "deleção" de dados na tabela de cadastro de produtos
+        cur.callproc( 'dlt_produto', (dlt_PRD_CODIGO ) )
+        conn.commit()
+        response = {"message": "Produto alterados com sucesso!"}
+    except Exception as e:
+        response = {"error": str(e)}
+        conn.rollback()
+    finally:
+        cur.close()
+        conn.close()
+    
+    return jsonify(response)
+
+########################    Seção de Produtos   ########################
+
+# Rota para adicionar um produto ao cadastro de produtos
+@app.route('/add_produto', methods=['POST'])
+def add_produto():
+    data = request.form
+    ad_PRD_CODIGO  = data.get('productName', '').strip()
+    ad_PRD_NOME    = data.get('productName', '').strip()
+    ad_PRD_PRECO   = float( data.get('productPrice', '').strip() )
+    ad_PRD_OBSERVA = data.get('productName', '').strip()
+
+    if not ad_PRD_NOME or not ad_PRD_PRECO:
+        return "Dados incompletos", 400
+    
+    conn = get_db_connection()
+    cur = conn.cursor()
+    
+    try:
+        # Chamando a procedure de inclusão de dados na tabela de cadastro de produtos
+        cur.callproc('add_produto', (ad_PRD_CODIGO, ad_PRD_NOME, ad_PRD_PRECO, ad_PRD_OBSERVA))
+        conn.commit()
+        response = {"message": "Produto adicionado com sucesso!"}
+    except Exception as e:
+        response = {"error": str(e)}
+        conn.rollback()
+    finally:
+        cur.close()
+        conn.close()
+    
+    return jsonify(response)
+
+
+# Rota para alterar um cadastro de um produto
+@app.route('/alte_produto', methods=['POST'])
+def alte_produto():
+    data = request.form
+    alt_PRD_CODIGO  = data.get('productName', '').strip()
+    alt_PRD_PRECO   = float( data.get('productPrice', '').strip() )
+    alt_PRD_OBSERVA = data.get('productName', '').strip()
+
+    if not alt_PRD_CODIGO or ( not alt_PRD_PRECO and not alt_PRD_OBSERVA ):
+        return "Dados incompletos", 400
+    
+    conn = get_db_connection()
+    cur = conn.cursor()
+    
+    try:
+        # Chamando a procedure de alteração de dados na tabela de cadastro de produtos
+        cur.callproc( 'alte_produto', (alt_PRD_CODIGO, alt_PRD_PRECO, alt_PRD_OBSERVA ) )
+        conn.commit()
+        response = {"message": "Produto alterados com sucesso!"}
+    except Exception as e:
+        response = {"error": str(e)}
+        conn.rollback()
+    finally:
+        cur.close()
+        conn.close()
+    
+    return jsonify(response)
+
+
+# Rota para deletar um cadastro de um produto
+@app.route('/dlt_produto', methods=['POST'])
+def dlt_produto():
+    data = request.form
+    dlt_PRD_CODIGO  = data.get('productName', '').strip()
+
+    if not dlt_PRD_CODIGO
+        return "Dados incompletos", 400
+    
+    conn = get_db_connection()
+    cur = conn.cursor()
+    
+    try:
+        # Chamando a procedure de "deleção" de dados na tabela de cadastro de produtos
+        cur.callproc( 'dlt_produto', (dlt_PRD_CODIGO ) )
+        conn.commit()
+        response = {"message": "Produto alterados com sucesso!"}
+    except Exception as e:
+        response = {"error": str(e)}
+        conn.rollback()
+    finally:
+        cur.close()
+        conn.close()
+    
+    return jsonify(response)
+
+########################    Seção de Produtos   ########################
+
+# Rota para adicionar um produto ao cadastro de produtos
+@app.route('/add_produto', methods=['POST'])
+def add_produto():
+    data = request.form
+    ad_PRD_CODIGO  = data.get('productName', '').strip()
+    ad_PRD_NOME    = data.get('productName', '').strip()
+    ad_PRD_PRECO   = float( data.get('productPrice', '').strip() )
+    ad_PRD_OBSERVA = data.get('productName', '').strip()
+
+    if not ad_PRD_NOME or not ad_PRD_PRECO:
+        return "Dados incompletos", 400
+    
+    conn = get_db_connection()
+    cur = conn.cursor()
+    
+    try:
+        # Chamando a procedure de inclusão de dados na tabela de cadastro de produtos
+        cur.callproc('add_produto', (ad_PRD_CODIGO, ad_PRD_NOME, ad_PRD_PRECO, ad_PRD_OBSERVA))
+        conn.commit()
+        response = {"message": "Produto adicionado com sucesso!"}
+    except Exception as e:
+        response = {"error": str(e)}
+        conn.rollback()
+    finally:
+        cur.close()
+        conn.close()
+    
+    return jsonify(response)
+
+
+# Rota para alterar um cadastro de um produto
+@app.route('/alte_produto', methods=['POST'])
+def alte_produto():
+    data = request.form
+    alt_PRD_CODIGO  = data.get('productName', '').strip()
+    alt_PRD_PRECO   = float( data.get('productPrice', '').strip() )
+    alt_PRD_OBSERVA = data.get('productName', '').strip()
+
+    if not alt_PRD_CODIGO or ( not alt_PRD_PRECO and not alt_PRD_OBSERVA ):
+        return "Dados incompletos", 400
+    
+    conn = get_db_connection()
+    cur = conn.cursor()
+    
+    try:
+        # Chamando a procedure de alteração de dados na tabela de cadastro de produtos
+        cur.callproc( 'alte_produto', (alt_PRD_CODIGO, alt_PRD_PRECO, alt_PRD_OBSERVA ) )
+        conn.commit()
+        response = {"message": "Produto alterados com sucesso!"}
+    except Exception as e:
+        response = {"error": str(e)}
+        conn.rollback()
+    finally:
+        cur.close()
+        conn.close()
+    
+    return jsonify(response)
+
+
+# Rota para deletar um cadastro de um produto
+@app.route('/dlt_produto', methods=['POST'])
+def dlt_produto():
+    data = request.form
+    dlt_PRD_CODIGO  = data.get('productName', '').strip()
+
+    if not dlt_PRD_CODIGO
+        return "Dados incompletos", 400
+    
+    conn = get_db_connection()
+    cur = conn.cursor()
+    
+    try:
+        # Chamando a procedure de "deleção" de dados na tabela de cadastro de produtos
+        cur.callproc( 'dlt_produto', (dlt_PRD_CODIGO ) )
+        conn.commit()
+        response = {"message": "Produto alterados com sucesso!"}
+    except Exception as e:
+        response = {"error": str(e)}
+        conn.rollback()
+    finally:
+        cur.close()
+        conn.close()
+    
+    return jsonify(response)
+
+
+##################################################################################################################################################
+##################################################################################################################################################
+##################################################################################################################################################
+##################################################################################################################################################
+
 
 @app.route('/cad_prod', methods=["POST"])
 def cad_prod():
