236cb8e1f9d75c3f47b388b77f34fa45735710e5
oscar donato <donato.oscar13@gmail.com>
2025-04-13 22:00:11 -0300

    ultimas alterações feitas com ate agora para fazer a integração.

--- main.py
+++ main.py
@@ -40,12 +40,12 @@ def get_db_connection():
 @app.route('/add_cliente', methods=['POST'])
 def add_cliente():
     data = request.form
-    ad_CLI_CODIGO = data.get('clientCPF', '').strip()
-    ad_CLI_NOME = data.get('clientName', '').strip()
+    ad_CLI_CODIGO   = data.get('clientCPF', '').strip()
+    ad_CLI_NOME     = data.get('clientName', '').strip()
     ad_CLI_ENDERECO	= data.get('clientAddress', '').strip() + data.get('clientComplement', '').strip() + data.get('clientCEP', '').strip()
-    ad_CLI_TEL = data.get('clientPhone', '').strip()
-    ad_CLI_DOC = data.get('clientCPF', '').strip()
-    ad_CLI_OBSERVA = data.get('clientNote', '').strip()
+    ad_CLI_TEL      = data.get('clientPhone', '').strip()
+    ad_CLI_DOC      = data.get('clientCPF', '').strip()
+    ad_CLI_OBSERVA  = data.get('clientNote', '').strip()
 
     if not ad_CLI_CODIGO or not ad_CLI_NOME or not ad_CLI_ENDERECO or not ad_CLI_TEL or not ad_CLI_DOC:
         return "Dados incompletos", 400
@@ -69,14 +69,15 @@ def add_cliente():
 
 
 # Rota para alterar um cadastro de um produto
-@app.route('/alte_produto', methods=['POST'])
-def alte_produto():
+@app.route('/alte_cliente', methods=['POST'])
+def alte_cliente():
     data = request.form
-    alt_PRD_CODIGO  = data.get('productName', '').strip()
-    alt_PRD_PRECO   = float( data.get('productPrice', '').strip() )
-    alt_PRD_OBSERVA = data.get('productName', '').strip()
+    alte_CLI_CODIGO   = data.get('clientCPF', '').strip()
+    alte_CLI_ENDERECO = data.get('clientAddress', '').strip() + data.get('clientComplement', '').strip() + data.get('clientCEP', '').strip()
+    alte_CLI_TEL      = data.get('clientPhone', '').strip()
+    alte_CLI_OBSERVA  = data.get('clientNote', '').strip()
 
-    if not alt_PRD_CODIGO or ( not alt_PRD_PRECO and not alt_PRD_OBSERVA ):
+    if not alte_CLI_CODIGO or ( not alte_CLI_ENDERECO and not alte_CLI_TEL ):
         return "Dados incompletos", 400
     
     conn = get_db_connection()
@@ -84,9 +85,9 @@ def alte_produto():
     
     try:
         # Chamando a procedure de alteração de dados na tabela de cadastro de produtos
-        cur.callproc( 'alte_produto', (alt_PRD_CODIGO, alt_PRD_PRECO, alt_PRD_OBSERVA ) )
+        cur.callproc( 'alte_cliente', (alte_CLI_CODIGO, alte_CLI_ENDERECO, alte_CLI_TEL, alte_CLI_OBSERVA ) )
         conn.commit()
-        response = {"message": "Produto alterados com sucesso!"}
+        response = {"message": "Cliente alterados com sucesso!"}
     except Exception as e:
         response = {"error": str(e)}
         conn.rollback()
@@ -98,12 +99,12 @@ def alte_produto():
 
 
 # Rota para deletar um cadastro de um produto
-@app.route('/dlt_produto', methods=['POST'])
-def dlt_produto():
+@app.route('/dlt_cliente', methods=['POST'])
+def dlt_cliente():
     data = request.form
-    dlt_PRD_CODIGO  = data.get('productName', '').strip()
+    dlt_CLI_CODIGO  = data.get('clientCPF', '').strip()
 
-    if not dlt_PRD_CODIGO:
+    if not dlt_CLI_CODIGO:
         return "Dados incompletos", 400
     
     conn = get_db_connection()
@@ -111,9 +112,9 @@ def dlt_produto():
     
     try:
         # Chamando a procedure de "deleção" de dados na tabela de cadastro de produtos
-        cur.callproc( 'dlt_produto', (dlt_PRD_CODIGO ) )
+        cur.callproc( 'dlt_cliente', (dlt_CLI_CODIGO ) )
         conn.commit()
-        response = {"message": "Produto alterados com sucesso!"}
+        response = {"message": "Cliente deletado com sucesso!"}
     except Exception as e:
         response = {"error": str(e)}
         conn.rollback()
@@ -200,7 +201,7 @@ def dlt_produto():
         # Chamando a procedure de "deleção" de dados na tabela de cadastro de produtos
         cur.callproc( 'dlt_produto', (dlt_PRD_CODIGO ) )
         conn.commit()
-        response = {"message": "Produto alterados com sucesso!"}
+        response = {"message": "Produto deletado com sucesso!"}
     except Exception as e:
         response = {"error": str(e)}
         conn.rollback()
@@ -210,18 +211,18 @@ def dlt_produto():
     
     return jsonify(response)
 
-########################    Seção de Produtos   ########################
+########################    Seção de Serviços   ########################
 
 # Rota para adicionar um produto ao cadastro de produtos
-@app.route('/add_produto', methods=['POST'])
-def add_produto():
+@app.route('/add_servico', methods=['POST'])
+def add_servico():
     data = request.form
-    ad_PRD_CODIGO  = data.get('productName', '').strip()
-    ad_PRD_NOME    = data.get('productName', '').strip()
-    ad_PRD_PRECO   = float( data.get('productPrice', '').strip() )
-    ad_PRD_OBSERVA = data.get('productName', '').strip()
+    ad_SRV_CODIGO  = data.get('serviceName', '').strip()
+    ad_SRV_NOME    = data.get('serviceName', '').strip()
+    ad_SRV_PRECO   = float( data.get('servicePrice', '').strip() )
+    ad_SRV_OBSERVA = data.get('serviceName', '').strip()
 
-    if not ad_PRD_NOME or not ad_PRD_PRECO:
+    if not ad_SRV_NOME or not ad_SRV_PRECO:
         return "Dados incompletos", 400
     
     conn = get_db_connection()
@@ -229,9 +230,9 @@ def add_produto():
     
     try:
         # Chamando a procedure de inclusão de dados na tabela de cadastro de produtos
-        cur.callproc('add_produto', (ad_PRD_CODIGO, ad_PRD_NOME, ad_PRD_PRECO, ad_PRD_OBSERVA))
+        cur.callproc('add_servico', (ad_SRV_CODIGO, ad_SRV_NOME, ad_SRV_PRECO, ad_SRV_OBSERVA))
         conn.commit()
-        response = {"message": "Produto adicionado com sucesso!"}
+        response = {"message": "Serviço adicionado com sucesso!"}
     except Exception as e:
         response = {"error": str(e)}
         conn.rollback()
@@ -243,14 +244,14 @@ def add_produto():
 
 
 # Rota para alterar um cadastro de um produto
-@app.route('/alte_produto', methods=['POST'])
-def alte_produto():
+@app.route('/alte_servico', methods=['POST'])
+def alte_servico():
     data = request.form
-    alt_PRD_CODIGO  = data.get('productName', '').strip()
-    alt_PRD_PRECO   = float( data.get('productPrice', '').strip() )
-    alt_PRD_OBSERVA = data.get('productName', '').strip()
+    alte_SRV_CODIGO  = data.get('serviceName', '').strip()
+    alte_SRV_PRECO   = float( data.get('servicePrice', '').strip() )
+    alte_SRV_OBSERVA = data.get('serviceName', '').strip()
 
-    if not alt_PRD_CODIGO or ( not alt_PRD_PRECO and not alt_PRD_OBSERVA ):
+    if not alte_SRV_CODIGO or ( not alte_SRV_PRECO and not alte_SRV_OBSERVA ):
         return "Dados incompletos", 400
     
     conn = get_db_connection()
@@ -258,9 +259,9 @@ def alte_produto():
     
     try:
         # Chamando a procedure de alteração de dados na tabela de cadastro de produtos
-        cur.callproc( 'alte_produto', (alt_PRD_CODIGO, alt_PRD_PRECO, alt_PRD_OBSERVA ) )
+        cur.callproc( 'alte_servico', (alte_SRV_CODIGO, alte_SRV_PRECO, alte_SRV_OBSERVA ) )
         conn.commit()
-        response = {"message": "Produto alterados com sucesso!"}
+        response = {"message": "Serviço alterados com sucesso!"}
     except Exception as e:
         response = {"error": str(e)}
         conn.rollback()
@@ -272,12 +273,12 @@ def alte_produto():
 
 
 # Rota para deletar um cadastro de um produto
-@app.route('/dlt_produto', methods=['POST'])
-def dlt_produto():
+@app.route('/dlt_servico', methods=['POST'])
+def dlt_servico():
     data = request.form
-    dlt_PRD_CODIGO  = data.get('productName', '').strip()
+    dlt_SRV_CODIGO  = data.get('serviceName', '').strip()
 
-    if not dlt_PRD_CODIGO:
+    if not dlt_SRV_CODIGO:
         return "Dados incompletos", 400
     
     conn = get_db_connection()
@@ -285,9 +286,9 @@ def dlt_produto():
     
     try:
         # Chamando a procedure de "deleção" de dados na tabela de cadastro de produtos
-        cur.callproc( 'dlt_produto', (dlt_PRD_CODIGO ) )
+        cur.callproc( 'dlt_servico', (dlt_SRV_CODIGO ) )
         conn.commit()
-        response = {"message": "Produto alterados com sucesso!"}
+        response = {"message": "Serviço deletado com sucesso!"}
     except Exception as e:
         response = {"error": str(e)}
         conn.rollback()
